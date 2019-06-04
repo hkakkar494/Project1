b3030d98bf6e5bacb512d91a4e784ad12c66cdb9
hkakkar494 <42166280+hkakkar494@users.noreply.github.com>
2019-06-04 11:42:22 +0100

    Add files via upload

---
+++ myClass1.java
@@ -0,0 +1,15 @@
+package Project1;
+import java.util.*;
+public class myClass1
+{
+	public static void main(String args[])
+	{
+		int a,b,c;
+		Scanner s1=new Scanner(System.in);
+		a=s1.nextInt();
+		b=s1.nextInt();
+		c=a+b;
+		System.out.print("Output is " + c);
+		s1.close();
+	}
+}
